498aee95e9d9ba80049c90d250790693528cd221
Ivan Barlog <ivan.barlog@beesolve.com>
2026-01-25 13:42:00 +0100

    revert default Node.js behaviour to buferred invoke mode

--- cdk.ts
+++ cdk.ts
@@ -80,7 +80,7 @@ type SvelteKitProps =
       readonly runtime: "node";
 
       /**
-       * @default InvokeMode.RESPONSE_STREAM
+       * @default InvokeMode.BUFFERED
        */
       readonly invokeMode?: InvokeMode;
 
@@ -131,7 +131,7 @@ export class SvelteKit extends Construct {
 
     const invokeMode =
       props.runtime === "node"
-        ? (props.invokeMode ?? InvokeMode.RESPONSE_STREAM)
+        ? (props.invokeMode ?? InvokeMode.BUFFERED)
         : InvokeMode.BUFFERED;
 
     const url = handler.addFunctionUrl({
